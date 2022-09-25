e950aa027b9f8c57fc2eacf6c19dd060d0cb50c7
vgangal <VGangal.ctr@kr.af.mil>
2022-09-24 21:42:00 -0400

    mods to the stable baselines approach

--- stable_baselines_test.py
+++ stable_baselines_test.py
@@ -4,8 +4,8 @@ from stable_baselines3 import DQN
 from stable_baselines3.common.env_util import make_atari_env
 from stable_baselines3.common.vec_env import VecFrameStack
 
-env1 = make_atari_env('PongNoFrameskip-v4', n_envs=4, seed=0)
+env1 = make_atari_env('PongNoFrameskip-v4', n_envs=1, seed=0)
 env2 = VecFrameStack(env1,n_stack=4)
-
+obs = env2.reset()
 
 
