87086b46751aa266ad1e5f79a2ee76cdb8f484ee
Vardaan Gangal <vardaan.gangal@gmail.com>
2022-10-02 04:00:51 +0000

    mods to graph utils

--- graphing_utils.py
+++ graphing_utils.py
@@ -3,12 +3,12 @@ import matplotlib.pyplot as plt
 
 
 def graph_training_ep_len(ep_len_tracked): 
-    x_vals = [i for i in range(len(ep_len_tracked))]
+    #x_vals = [i for i in range(len(ep_len_tracked))]
     plt.plot(ep_len_tracked)
     plt.savefig('episode_length_graph.png')
 
 def graph_training_rewards(rewards_tracked): 
-    x_vals = [i for i in range(len(rewards_tracked))]
+    #x_vals = [i for i in range(len(rewards_tracked))]
     #plt.plot(rewards_tracked)
     plt.plot(rewards_tracked)
     plt.savefig('rewards_graph.png')
